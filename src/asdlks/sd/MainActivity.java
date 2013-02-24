e956882d6b35a3ff03faafff630db718541f1503
mik403 <mik403@yandex.ru>
2013-02-25 00:55:20 +0200

    first start

---
+++ src/asdlks/sd/MainActivity.java
@@ -0,0 +1,48 @@
+package asdlks.sd;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    Storage st;
+    /**
+     * Called when the activity is first created.
+     */
+    public void updateView() {
+        TextView text = (TextView) findViewById(R.id.text1);
+        text.setText(st.getCurrQuection());
+        text = (TextView) findViewById(R.id.text2);
+        text.setText("");
+    }
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+
+        setContentView(R.layout.main);
+        
+        st = new Storage();
+        TextView text = (TextView) findViewById(R.id.text1);
+        text.setText(st.getCurrQuection());
+    }
+
+    public void onClick(View view) {
+
+        TextView text = (TextView) findViewById(R.id.text2);
+        text.setText(st.getCurrAnswer());
+    }
+    public void onClickLeft(View view) {
+        st.moveLeft();
+        updateView();
+    }
+    
+    public void onClickRight(View view) {
+        st.moveRight();
+        updateView(); 
+    }
+}
