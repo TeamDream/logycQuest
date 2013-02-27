bfd7f1e471549d434c2ba79fddd8e533761a577b
dshevchyk <shevchykdmytro@gmail.com>
2013-02-27 22:37:37 +0200

    renamed packege and folders

--- src/dreamteam/logicquest/MainActivity.java
+++ src/dreamteam/logicquest/MainActivity.java
@@ -1,9 +1,10 @@
-package asdlks.sd;
+package dreamteam.logicquest;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
 
 public class MainActivity extends Activity {
     Storage st;
